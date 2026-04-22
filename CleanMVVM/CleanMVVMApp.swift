4afeb0e8d64ceea41fac6545b98abe98f32e075c
Anatolii <anatolii.semenchuk@schindler.com>
2026-04-22 16:09:06 +0300

    Initial Commit

---
+++ CleanMVVM/CleanMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanMVVMApp.swift
+//  CleanMVVM
+//
+//  Created by Anatolii Semenchuk on 22.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct CleanMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
